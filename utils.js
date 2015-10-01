933361ae3fd5ec6274e24a9b8c86f240f78dc9c1
Marat Habibullin <maratx387@gmail.com>
2015-10-01 17:43:07 +0300

    Keyboard settings changed

---
+++ utils.js
@@ -0,0 +1,18 @@
+var utils = {
+    KEY_W : 87,
+    KEY_A : 65,
+    KEY_S : 83,
+    KEY_D : 68,
+    
+    KEY_I : 73,
+    KEY_J : 74,
+    KEY_K : 75,
+    KEY_L : 76,
+
+    KEY_Z : 90,
+    KEY_X : 88,
+
+    degToRad : function (degrees) {
+        return degrees * Math.PI / 180;
+    }
+};
